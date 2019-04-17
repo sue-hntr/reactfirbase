1ea9b71349cccde2b853631701b1eca318e0c21e
Suzanne Hunter <sue_huntr@yahoo.com>
2019-04-16 21:18:45 -0400

    Begin Session Handling

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+    </FirebaseContext.Provider>,
+    document.getElementById("root"),
+);
